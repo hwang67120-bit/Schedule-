c6de5c559af8a57d26d05137f1424937569547c7
hwang67120 <hwang67120@gmail.com>
2026-04-13 19:57:02 +0900

    컨트롤 수정

--- src/main/java/com/nodeajva/schedule/controller/ScheduleController.java
+++ src/main/java/com/nodeajva/schedule/controller/ScheduleController.java
@@ -1,11 +1,14 @@
 package com.nodeajva.schedule.controller;
 
+import com.nodeajva.schedule.dto.ScheduleRequest;
+import com.nodeajva.schedule.dto.ScheduleResponse;
 import com.nodeajva.schedule.entity.ScheduleEntity;
 import com.nodeajva.schedule.service.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/schedules")
@@ -16,32 +19,40 @@ public class ScheduleController {
 
     //등록
     @PostMapping
-    public ScheduleEntity save (@RequestBody ScheduleEntity schedule) {
-        return scheduleService.save(schedule);
+    public ScheduleResponse save(@RequestBody ScheduleRequest request) {
+        ScheduleEntity entity = request.toEntity();
+        ScheduleEntity saved = scheduleService.save(entity);
+        return ScheduleResponse.from(saved);
     }
 
     //전체조회
     @GetMapping
-    public List<ScheduleEntity> findAll(
+    public List<ScheduleResponse> findAll(
             @RequestParam(required = false) String name
     ) {
-        return scheduleService.findAll(name);
+        return scheduleService.findAll(name).stream()
+                .map(ScheduleResponse::from)
+                .collect(Collectors.toList());
     }
 
     //단건 조회
     @GetMapping("/{id}")
-    public ScheduleEntity findBy(@PathVariable Long id) {
-        return scheduleService.findById(id);
+    public ScheduleResponse findBy(@PathVariable Long id) {
+        ScheduleEntity entity = scheduleService.findById(id);
+
+        return ScheduleResponse.from(entity);
     }
 
     //수정
     @PutMapping("/{id}")
-    public ScheduleEntity update(
+    public ScheduleResponse update(
             @PathVariable Long id,
             @RequestParam String password,
-            @RequestBody ScheduleEntity schedule
-    ){
-        return scheduleService.update(id, password, schedule);
+            @RequestBody ScheduleRequest request
+    ) {
+        ScheduleEntity entity = request.toEntity();
+        ScheduleEntity updated = scheduleService.update(id, password, entity);
+        return ScheduleResponse.from(updated);
     }
 
     @DeleteMapping("/{id}")
