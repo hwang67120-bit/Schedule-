783c2a80933efb7951848b642d7327a6d88290e2
hwang67120 <hwang67120@gmail.com>
2026-04-13 19:20:03 +0900

    서비스 시간생성 추가

--- src/main/java/com/nodeajva/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/nodeajva/schedule/repository/ScheduleRepository.java
@@ -3,6 +3,11 @@ package com.nodeajva.schedule.repository;
 import com.nodeajva.schedule.entity.ScheduleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Long> {
 
+    List<ScheduleEntity> findAllByOrderByUpdateAtDesc();
+
+    List<ScheduleEntity> findByNameOrderByUpdateAtDesc(String name);
 }
