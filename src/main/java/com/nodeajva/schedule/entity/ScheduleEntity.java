a53580c5d72f251499f795f8932bb258250e4324
hwang67120 <hwang67120@gmail.com>
2026-04-13 15:06:43 +0900

    스케줄엔티티

--- src/main/java/com/nodeajva/schedule/entity/ScheduleEntity.java
+++ src/main/java/com/nodeajva/schedule/entity/ScheduleEntity.java
@@ -1,4 +1,51 @@
 package com.nodeajva.schedule.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "schedule")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ScheduleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String title;
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime creatAt;
+
+    @Column
+    private LocalDateTime updateAt;
+
+    @PrePersist
+    protected void onCreat(){
+        creatAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        updateAt = LocalDateTime.now();
+    }
 }
